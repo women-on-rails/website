37967d0827da6cdbc56237e71f9349856df31956
Lisa Durand <lisa@superdudu.net>
2016-05-22 19:20:38 +0200

    setup meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def default_url_options
+    if Rails.env.production?
+      { host: 'http://womenonrails.com/' }
+    else
+      { host: ENV['HOST'] || 'localhost:3000' }
+    end
+  end
 end
